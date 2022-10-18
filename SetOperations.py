b5785c3826ba1cbfaa8e454fa5c222e4304b0ce0
GudeNithin <95757938+GudeNithin@users.noreply.github.com>
2022-10-18 22:29:23 +0530

    Create SetOperations.py

---
+++ SetOperations.py
@@ -0,0 +1,16 @@
+#Creating a Set-set1
+set1={1,2,4,6}
+#creating another Set-set2
+set2={8,2,6,1}
+#this indicates union
+a=set1|set2
+print(a)
+#indicates intersection
+b=set1&set2
+print(b)
+#difference
+c=set1-set2
+print(c)
+#symmetric difference
+d=set1^set2
+print(d)
